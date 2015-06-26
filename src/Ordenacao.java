ff92af7ae0a11c796071024b1806e3225b144595
Fagner Cunha <francisco.cunha@iatecam.org.br>
2015-06-26 08:13:01 -0400

    Algoritmo insertion sort

--- src/Ordenacao.java
+++ src/Ordenacao.java
@@ -1,4 +1,20 @@
-
 public class Ordenacao {
+
+	public static void insertion(int[] num) {
+		int j; 
+		int key; 
+		int i;
+
+		for (j = 1; j < num.length; j++) // Start with 1 (not 0)
+		{
+			key = num[j];
+			for (i = j - 1; (i >= 0) && (num[i] < key); i--) // Smaller values
+																// are moving up
+			{
+				num[i + 1] = num[i];
+			}
+			num[i + 1] = key; // Put the key in its proper location
+		}
+	}
 
 }
