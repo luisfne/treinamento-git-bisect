03dbed874262fd5060b03f57d4f29e399fb4f6fe
Kaleb <kaleb.silva@iatecam.org.br>
2015-06-26 08:16:54 -0400

    Algoritmo bubble sort

--- src/Ordenacao.java
+++ src/Ordenacao.java
@@ -35,4 +35,21 @@ public class Ordenacao {
 		}
 	}
 
+	public static int[] bubble(int[] vetor) {
+		boolean troca = true;
+		int aux;
+		while (troca) {
+			troca = false;
+			for (int i = 0; i < vetor.length - 1; i++) {
+				if (vetor[i] > vetor[i + 1]) {
+					aux = vetor[i];
+					vetor[i] = vetor[i + 1];
+					vetor[i + 1] = aux;
+					troca = true;
+				}
+			}
+		}
+		return vetor;
+	}
+
 }
