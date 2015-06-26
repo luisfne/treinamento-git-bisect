d1c52325f2acb44f05a9c91f49211261b2f80cf4
Israel <israel.marinho@iatecam.org.br>
2015-06-26 08:14:23 -0400

    Algoritmo selection sort

--- src/Ordenacao.java
+++ src/Ordenacao.java
@@ -1,8 +1,8 @@
 public class Ordenacao {
 
 	public static void insertion(int[] num) {
-		int j; 
-		int key; 
+		int j;
+		int key;
 		int i;
 
 		for (j = 1; j < num.length; j++) // Start with 1 (not 0)
@@ -14,6 +14,24 @@ public class Ordenacao {
 				num[i + 1] = num[i];
 			}
 			num[i + 1] = key; // Put the key in its proper location
+		}
+	}
+
+	public static void selection(int[] array) {
+		for (int fixo = 0; fixo < array.length - 1; fixo++) {
+			int menor = fixo;
+
+			for (int i = menor + 1; i < array.length; i++) {
+				if (array[i] < array[menor]) {
+					menor = i;
+				}
+			}
+			if (menor != fixo) {
+				// Troca
+				int t = array[fixo];
+				array[fixo] = array[menor];
+				array[menor] = t;
+			}
 		}
 	}
 
