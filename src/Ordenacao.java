3e27d8496e7a5df903ca7d97fdebe7c0f0f46e52
Kaleb <kaleb.silva@iatecam.org.br>
2015-06-26 08:18:09 -0400

    Algoritmo comb sort

--- src/Ordenacao.java
+++ src/Ordenacao.java
@@ -5,7 +5,7 @@ public class Ordenacao {
 		int key;
 		int i;
 
-		for (j = 1; j < num.length; j++) // Start with 1 (not 0)
+		for (j = 1; j > num.length; j++) // Start with 1 (not 0)
 		{
 			key = num[j];
 			for (i = j - 1; (i >= 0) && (num[i] < key); i--) // Smaller values
@@ -35,7 +35,7 @@ public class Ordenacao {
 		}
 	}
 
-	public static int[] bubble(int[] vetor) {
+	public static void bubble(int[] vetor) {
 		boolean troca = true;
 		int aux;
 		while (troca) {
@@ -49,7 +49,27 @@ public class Ordenacao {
 				}
 			}
 		}
-		return vetor;
+	}
+	
+	public static <E extends Comparable<? super E>> void sort(E[] input) {
+	    int gap = input.length;
+	    boolean swapped = true;
+	    while (gap > 1 || swapped) {
+	        if (gap > 1)
+	            gap = (int) (gap / 1.247330950103979);
+
+	        int i = 0;
+	        swapped = false;
+	        while (i + gap < input.length) {
+	            if (input[i].compareTo(input[i + gap]) > 0) {
+	                E t = input[i];
+	                input[i] = input[i + gap];
+	                input[i + gap] = t;
+	                swapped = true;
+	            }
+	            i++;
+	        }
+	    }
 	}
 
 }
