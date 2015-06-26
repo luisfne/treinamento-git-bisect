a891234fc38c825fc57e245343b169c4a5553712
Ricardo Erikson <ricardo.rosa@iatecam.org.br>
2015-06-26 08:00:28 -0400

    Método de busca binária adicionado

--- src/Busca.java
+++ src/Busca.java
@@ -1,4 +1,23 @@
-
 public class Busca {
+
+	public static int binaria(int[] array, int valor) {
+		int inicio = 0;
+		int fim = array.length - 1;
+
+		while (inicio <= fim) {
+			int meio = (inicio + fim) / 2;
+
+			if (array[meio] == valor) {
+				return meio;
+			}
+
+			if (valor > array[meio]) {
+				inicio = meio + 1;
+			} else {
+				fim = meio - 1;
+			}
+		}
+		return -1;
+	}
 
 }
