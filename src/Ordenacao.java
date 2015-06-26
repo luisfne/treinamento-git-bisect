afa0fb09d470970e66c8b06a6de8d7de06345e26
Ricardo Erikson <ricardo.rosa@iatecam.org.br>
2015-06-26 08:19:32 -0400

    Algoritmo merge sort

--- src/Ordenacao.java
+++ src/Ordenacao.java
@@ -50,26 +50,58 @@ public class Ordenacao {
 			}
 		}
 	}
-	
-	public static <E extends Comparable<? super E>> void sort(E[] input) {
-	    int gap = input.length;
-	    boolean swapped = true;
-	    while (gap > 1 || swapped) {
-	        if (gap > 1)
-	            gap = (int) (gap / 1.247330950103979);
 
-	        int i = 0;
-	        swapped = false;
-	        while (i + gap < input.length) {
-	            if (input[i].compareTo(input[i + gap]) > 0) {
-	                E t = input[i];
-	                input[i] = input[i + gap];
-	                input[i + gap] = t;
-	                swapped = true;
-	            }
-	            i++;
-	        }
-	    }
+	public static <E extends Comparable<? super E>> void sort(E[] input) {
+		int gap = input.length;
+		boolean swapped = true;
+		while (gap > 1 || swapped) {
+			if (gap > 1)
+				gap = (int) (gap / 1.247330950103979);
+
+			int i = 0;
+			swapped = false;
+			while (i + gap < input.length) {
+				if (input[i].compareTo(input[i + gap]) > 0) {
+					E t = input[i];
+					input[i] = input[i + gap];
+					input[i + gap] = t;
+					swapped = true;
+				}
+				i++;
+			}
+		}
+	}
+
+	public static void mergeSort(int[] array, int inicio, int fim) {
+		if (fim <= inicio) {
+			return;
+		}
+		int meio = (inicio + fim) / 2;
+		mergeSort(array, inicio, meio);
+		mergeSort(array, meio + 1, fim);
+		int[] A = new int[meio - inicio + 1];
+		int[] B = new int[fim - meio];
+		for (int i = 0; i <= meio - inicio; i++) {
+			A[i] = array[inicio + i];
+		}
+		for (int i = 0; i <= fim - meio - 1; i++) {
+			B[i] = array[meio + 1 + i];
+		}
+		int i = 0;
+		int j = 0;
+		for (int k = inicio; k <= fim; k++) {
+			if (i < A.length && j < B.length) {
+				if (A[i] < B[j]) {
+					array[k] = A[i++];
+				} else {
+					array[k] = B[j++];
+				}
+			} else if (i < A.length) {
+				array[k] = A[i++];
+			} else if (j < B.length) {
+				array[k] = B[j++];
+			}
+		}
 	}
 
 }
